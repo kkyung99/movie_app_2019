963958e244273acbcb25fb825d1c5831f2eaeae2
unknown <becky0508@naver.com>
2020-07-24 17:32:50 +0900

    Updates

---
+++ precache-manifest.4b13ae03a8ff707228f8a1a0fdf33a22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0393af2a585ff7586f3a00b92ffeb339",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e8b46ef485ea64268e2f",
+    "url": "/movie_app_2019/static/css/main.462454f0.chunk.css"
+  },
+  {
+    "revision": "fde87d2ad6cabe52f608",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019/static/js/2.c3d73534.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8b46ef485ea64268e2f",
+    "url": "/movie_app_2019/static/js/main.71c27452.chunk.js"
+  },
+  {
+    "revision": "059a9ab2adfa282ae316",
+    "url": "/movie_app_2019/static/js/runtime-main.12568eff.js"
+  }
+]);
